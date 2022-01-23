536bb8517f83e52fe906b8aaf08b2b8c191175f1
Lila Papazian <lilapapazian@gmail.com>
2022-01-23 18:38:10 -0300

    add webpack customization

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
 }
+
