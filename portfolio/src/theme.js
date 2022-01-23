08f0dc2bf83408b4bb947f1b0133721b71099b43
Lila Papazian <lilapapazian@gmail.com>
2022-01-23 18:37:30 -0300

    add text color effect

--- portfolio/src/theme.js
+++ portfolio/src/theme.js
@@ -18,6 +18,18 @@ const theme = createTheme({
     }
   },
   padding: 0,
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          backgroundImage: 'linear-gradient(90deg, #7700ea, #6d19e9, #6426e7, #5a2fe5, #5035e3, #463be0, #3c3fdd, #3243da, #2846d6, #1c49d2, #104cce, #004eca)',
+          backgroundClip: "text",
+          textFillColor: "transparent"
+        }
+      },
+    }
+  }
+
 });
 
 export default theme;
