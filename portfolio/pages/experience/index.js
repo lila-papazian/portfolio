f8ed533f741f7a7086195d47c006a83e0845ff67
Lila Papazian <lilapapazian@gmail.com>
2022-01-27 19:44:22 -0300

    reorganize page structure to make routing work

---
+++ portfolio/pages/experience/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import TopBarContainer from "../../src/components/TopBarContainer.tsx"
+
+export default function Experience() {
+  return (
+    <Container disableGutters>
+      <TopBarContainer />
+    </Container>
+  );
+}
---
+++ portfolio/pages/projects/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import TopBarContainer from "../../src/components/TopBarContainer.tsx"
+
+export default function Projects() {
+  return (
+    <Container disableGutters>
+      <TopBarContainer />
+    </Container>
+  );
+}
