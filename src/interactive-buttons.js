46b53ee971dbf1ec6532b42ad9004de2aaeb6304
Lila Papazian <lilapapazian@gmail.com>
2024-10-10 23:13:25 -0300

    add toggle sounds

--- src/interactive-buttons.js
+++ src/interactive-buttons.js
@@ -1,35 +1,66 @@
 const gameOverSound = new Audio("/game-over.mp3");
 const jumpSound = new Audio("/jump.mp3");
+const powerDownSound = new Audio("/power-down.mp3");
+const powerUpSound = new Audio("/power-up.mp3");
 
 const goToStats = document.getElementById("go-to-stats");
 const startGame = document.getElementById("start-game");
 const endGame = document.getElementById("game-over");
 const statsSection = document.getElementById("stats");
 
+const toggleSoundButton = document.getElementById("toggle-sound");
+const soundIcon = document.getElementById("sound-icon");
+let soundEnabled = true;
+
+const toggleSound = () => {
+  soundEnabled = !soundEnabled;
+  soundIcon.src = soundEnabled ? "/high-volume.png" : "/mute.png";
+
+  if (soundEnabled) {
+    soundIcon.alt = "Sound On";
+    powerUpSound.play();
+  } else {
+    soundIcon.alt = "Sound Off";
+    powerDownSound.play();
+  }
+};
+
+toggleSoundButton.addEventListener("click", toggleSound);
+
 goToStats?.addEventListener("click", () => {
-  jumpSound.play();
+  if (soundEnabled) {
+    jumpSound.play();
+  }
   statsSection?.scrollIntoView();
 });
 
 startGame?.addEventListener("click", () => {
-  jumpSound.play();
+  if (soundEnabled) {
+    jumpSound.play();
+  }
   statsSection?.scrollIntoView();
 });
 
 endGame?.addEventListener("click", () => {
-  gameOverSound.play();
+  if (soundEnabled) {
+    gameOverSound.play();
+  }
 });
 
 const goToResume = document.getElementById("go-to-resume");
 const resumeSection = document.getElementById("resume");
 goToResume?.addEventListener("click", () => {
-  jumpSound.play();
+  if (soundEnabled) {
+    jumpSound.play();
+  }
   resumeSection?.scrollIntoView();
 });
 
 const goToContact = document.getElementById("go-to-contact");
 const contactSection = document.getElementById("contact");
 goToContact?.addEventListener("click", () => {
-  jumpSound.play();
+  if (soundEnabled) {
+    jumpSound.play();
+  }
   contactSection?.scrollIntoView();
 });
