7d47f6e87cb62d6659b0f2b14d920581d9ca9552
Lila Papazian <lilapapazian@gmail.com>
2026-09-14 21:19:28 -0300

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   // integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
